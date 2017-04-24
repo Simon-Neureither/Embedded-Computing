9c7426b4f4afdb007e4c1c56633ad076d5f6e16f
Simon-Neureither <simonneureither@gmail.com>
2017-04-24 15:03:56 +0200

    Aufgabe2

--- Aufgabe2/Aufgabe2/Aufgabe2.c
+++ Aufgabe2/Aufgabe2/Aufgabe2.c
@@ -2,10 +2,27 @@
 #include <stdio.h>
 
 #include <time.h>
+#include <sys/neutrino.h>
+#include <errno.h>
 
+int clockMeasure();
 void changeSystemTick(unsigned int microsecs);
 
+
 int main(int argc, char *argv[]) {
+	changeSystemTick(536869);
+	int ret = clockMeasure();
+	if (ret == EXIT_FAILURE) return ret;
+	//changeSystemTick(10);
+	//clockMeasure();
+	//if (ret == EXIT_FAILURE) return ret;
+	changeSystemTick(1000);
+
+	return EXIT_SUCCESS;
+}
+
+int clockMeasure()
+{
 	int ret;
 	struct timespec startTime;
 	struct timespec timeOld;
@@ -45,7 +62,7 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	ret = clock_gettime(CLOCK_REALTIME, &timeNew);
+	ret = clock_gettime(CLOCK_REALTIME, &timeNew); // TODO
 	long seconds = timeNew.tv_sec - startTime.tv_sec;
 	long nanos = timeNew.tv_nsec - startTime.tv_nsec;
 	if (nanos < 0)
@@ -53,14 +70,41 @@ int main(int argc, char *argv[]) {
 		nanos+=BILLION;
 		seconds--;
 	}
-	printf("%lu:%lu", seconds, nanos);
-
-	return EXIT_SUCCESS;
+	printf("%lu:%lu\r\n", seconds, nanos);
 }
 
 void changeSystemTick(unsigned int microsecs)
 {
+	struct timespec res;
+	int ret = clock_getres(CLOCK_REALTIME, &res);
 
-}
+	if (ret == -1)
+	{
+		perror("clock_getres");
+		exit(EXIT_FAILURE);
+	}
 
+	printf("Old: %lu\n", res.tv_nsec);
+
+	struct _clockperiod NewClockPeriod;
+
+	NewClockPeriod.nsec = microsecs * 1000;
+	NewClockPeriod.fract = 0;
+
+	ret = ClockPeriod_r(CLOCK_REALTIME, &NewClockPeriod, NULL, 0);
+
+	if (ret != EOK)
+	{
+		fprintf(stderr, "error: %s\n", strerror(ret));
+		exit(EXIT_FAILURE);
+	}
+
+	ret = clock_getres(CLOCK_REALTIME, &res);
+	if (ret == -1)
+	{
+		perror("clock_getres");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("New: %lu\n", res.tv_nsec);
 }
